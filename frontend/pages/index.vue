d5dbe0997902aa6147e4435d413d5c702e156783
kiha40-777 <y.toriyama183501@gmail.com>
2025-07-07 16:37:57 +0900

    edit

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,16 +4,15 @@
       <h1 class="text-4xl font-bold mb-4 mt-8">AI手書き数字認識</h1>
       <p class="text-lg mb-8">自分が何の数字を書いたかAIに当ててもらおう！</p>
     </div>
-    <div class="flex items-center justify-center">
+    <div class="flex items-center justify-center w-full h-full">
       <div class="p-4 flex">
-        <div class="text-center items-center mt-4 w-1/2 p-4">
+        <div class="text-center items-center mt-4 p-4">
           <p class="text-lg mb-4 text-left">↓数字を書いてみよう！</p>
           <CanvasDrawer />
         </div>
-        <div class="text-center items-center mt-4 w-1/2 p-4">
+        <div class="text-center items-center mt-4 p-4 flex-none w-[400px]">
           <p class="text-lg mb-4 text-left">　</p>
-          <p>手書きの数字をキャンバスに描いて、予測ボタンを押してください。</p>
-          <p>予測結果と確信度が表示されます。</p>
+          <h1 class="text-2xl font-bold mt-2">AIが考えた結果</h1>
         </div>
       </div>
     </div>
